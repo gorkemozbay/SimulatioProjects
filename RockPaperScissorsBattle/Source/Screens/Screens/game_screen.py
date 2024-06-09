1a7b09dfd5b3ceee1b712c5fdbe55ab12927d1de
gorkemozbay <gorkemozbay0405@gmail.com>
2024-06-09 17:07:12 +0300

    Minor edits.

--- RockPaperScissorsBattle/Source/Screens/Screens/game_screen.py
+++ RockPaperScissorsBattle/Source/Screens/Screens/game_screen.py
@@ -25,23 +25,42 @@ class GameScreen(ScreenBase):
         self.object_counts = self.object_manager.update_objects()
 
 
+    def render(self, screen):
+        screen.fill(BACKGROUND)
+        self.object_manager.render_objects(screen)
+        self.render_UI(screen)
+        
+        
     def render_UI(self, screen):
         pygame.draw.line(screen, WHITE, (0, OFF_SET_POS), (MAP_WIDTH, OFF_SET_POS), 2)
-        self.draw_text(screen, f"Rock: {self.object_counts[0]}", WHITE, (MAP_WIDTH * 0.05, 10))
-        self.draw_text(screen, f"Paper: {self.object_counts[1]}", WHITE, (MAP_WIDTH * 0.40, 10))
-        self.draw_text(screen, f"Scissor: {self.object_counts[2]}", WHITE, (MAP_WIDTH * 0.75, 10))
+        self.draw_text(screen, f"Rock: {self.object_counts[0]}",    WHITE, (MAP_WIDTH * 0.15, 20))
+        self.draw_text(screen, f"Paper: {self.object_counts[1]}",   WHITE, (MAP_WIDTH * 0.45, 20))
+        self.draw_text(screen, f"Scissor: {self.object_counts[2]}", WHITE, (MAP_WIDTH * 0.80, 20))
+        is_done, idx = self.check_win_conditions()
+        if is_done:
+            self.draw_text(screen, f"{self.idx_to_object(idx)} is WINNER", WHITE, (MAP_WIDTH * 0.5, MAP_HEIGHT * 0.5))
 
-    
+
     def draw_text(self, screen, text, color, pos):
         text_surface = pygame.font.Font(None, 36).render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.topleft = pos
+        text_rect.center = pos
         screen.blit(text_surface, text_rect)
-
+        
     
-    def render(self, screen):
-        screen.fill(BACKGROUND)
-        self.render_UI(screen)
-        self.object_manager.render_objects(screen)
+    def check_win_conditions(self):
+        for idx, count in enumerate(self.object_counts):
+            if count == NUMBER_OF_OBJ_PER_CLASS * 3:
+                return True, idx
+        return False, 0
+    
 
-                
+    def idx_to_object(self, idx):
+        if idx == 0:
+            return "Rock"
+        elif idx == 1:
+            return "Paper"
+        elif idx == 2:
+            return "Scissor"
+        else:
+            return "Error"
